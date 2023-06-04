64e6c01dd9e0d6cc9d7fae03308be0e90823133e
Mehmet Emin Çetin <114653115+mehmetemincetin@users.noreply.github.com>
2023-06-05 00:42:13 +0300

    Update Algo2-WhilePrintfNumbers.c

--- Day1/Algo2-WhilePrintfNumbers.c
+++ Day1/Algo2-WhilePrintfNumbers.c
@@ -9,4 +9,6 @@ int main() {
     }
     return 0;
 }
-
+/*Özet olarak bu algoritma 1'den başlayarak 10 kadar olan sayıların çıktılarını ekrana yazdırır ve burada while fonksiyonundan faydalanır 
+while döngüsü üstündeki köntrölü yapıp her döngüde parantez içindeki denklemi sağladığı sürece süslü parantez içerisindeki işlemleri devam ettirir.
+*/
