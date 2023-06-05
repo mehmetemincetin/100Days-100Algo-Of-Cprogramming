4a7bc052dc1cd95fa448d1ecbd342fdb47c471d1
Mehmet Emin Çetin <114653115+mehmetemincetin@users.noreply.github.com>
2023-06-05 22:39:20 +0300

    Update Algo11-WhileReversed.c

--- Day2/Algo11-WhileReversed.c
+++ Day2/Algo11-WhileReversed.c
@@ -12,3 +12,9 @@ int main() {
     printf("The reverse of the number you entered: %d",reversed);
     return 0;
 }
+/*
+In this algorithm,I first defined the number and reverse variables as int.
+Then I got the number from the user Then I using while loop
+reversed = reversed * 10 + num % 10, num = num / 10.
+Then I printed on the screen
+*/
